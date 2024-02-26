fcaa33a9597172de253ccd098963a4985152eb6b
yumarie <yumarie@umich.edu>
2024-02-26 00:06:07 -0500

    fix schema init script

--- worker/migrations/000-init.sql
+++ worker/migrations/000-init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS date_entries (
     e_id                                INTEGER PRIMARY KEY AUTOINCREMENT,
     date                                TEXT UNIQUE NOT NULL, 
-    articles_published,                 INTEGER NOT NULL,
+    articles_published                  INTEGER NOT NULL,
     images_published                    INTEGER NOT NULL,
     images_published_with_alt_text      INTEGER NOT NULL,
     category_data                       TEXT NOT NULL,
