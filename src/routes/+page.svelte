2b7f7de634859e1507e3bf90f7c5d983a99f74dc
yumarie <yumarie@umich.edu>
2024-03-04 03:28:23 -0500

    first draft of stacked bar chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,56 +2,96 @@
 	import * as d3 from 'd3';
 	import type { DateEntry } from '$lib/types';
 
-	export let data:{entries: Array<DateEntry>}|null;
+	export let data: { entries: Array<DateEntry> } | null;
 	let entries = data?.entries ?? [];
 
-	const tidy = entries.map((d:DateEntry) => [
-		{
-			date: d.date,
-			status: "Images published with alternative text",
-			value: d.images_published_with_alt_text
-		},
-		{
-			date: d.date,
-			status: "Images published without alternative text",
-			value: d.images_published - d.images_published_with_alt_text
-		}
-	]).flat()
+	$: timerange = all;
 
-	const index = d3.rollup(
-		tidy,
-		(v) => Object.fromEntries(v.map((o) => [o.status, o.value])),
-		(d) => d.date
-	);
+	const lastWeek = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
+	const lastMonth = new Date(new Date().getTime() - 7 * 4 * 24 * 60 * 60 * 1000);
+	const lastYear = new Date(new Date().getTime() - 365 * 24 * 60 * 60 * 1000);
+	const all = new Date('2022-12-31');
 
-	const color = d3.scaleOrdinal()
-		.domain([
-		"Images published with alternative text",
-		"Images published without alternative text",
-		])
-		.range(["lightgreen", "LightCoral"]);
+	$: category = 0;
 
+	const gap = 1;
 	const padding = 32;
 	$: innerWidth = 1300;
 	$: innerHeight = 600;
 	$: width = innerWidth / 1.1;
 	$: height = innerHeight / 1.75;
 
-	const lastWeek = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
-	const lastMonth = new Date(new Date().getTime() - 7 * 4 * 24 * 60 * 60 * 1000);
-	const lastYear = new Date(new Date().getTime() - 365 * 24 * 60 * 60 * 1000);
-	$: timerange = lastYear;
+	let tidy = entries
+		.map((d: DateEntry) => [
+			{
+				date: d.date,
+				status: 'Images published with alternative text',
+				value: d.images_published_with_alt_text
+			},
+			{
+				date: d.date,
+				status: 'Images published',
+				value: d.images_published
+			}
+		])
+		.flat();
 
-	$: category = 0;
+	$: tidy = entries
+		.map((d: DateEntry) => [
+			{
+				date: d.date,
+				status: 'Images published with alternative text',
+				value: d.images_published_with_alt_text
+			},
+			{
+				date: d.date,
+				status: 'Images published',
+				value: d.images_published
+			}
+		])
+		.flat()
+		.filter((entry) => entry.date > timerange.toISOString().split('T')[0]);
+
+	$: index = d3.rollup(
+		tidy,
+		(v) => Object.fromEntries(v.map((o) => [o.status, o.value])),
+		(d) => d.date
+	);
+
+	$: x = d3.scaleUtc(d3.extent(tidy.map((d) => new Date(d.date))) as Iterable<Number>, [
+		padding,
+		width - padding
+	]);
+	$: y = d3.scaleLinear(d3.extent(tidy.map((d) => d.value)) as Iterable<Number>, [
+		height - padding,
+		padding
+	]);
 
 	let gx: SVGGElement;
 	let gy: SVGGElement;
-
-	$: x = d3.scaleUtc([timerange, new Date()], [padding, width - padding]);
-	$: y = d3.scaleLinear(d3.extent(entries.map((d) => d.images_published)) as Iterable<Number>, [height - padding, padding]);
-
 	$: d3.select(gx).call(d3.axisBottom(x));
 	$: d3.select(gy).call(d3.axisLeft(y));
+
+	// Function to handle mouseover event
+	function handleMouseOver(d, data) {
+		// Get mouse coordinates
+		let [xPos, yPos] = d3.pointer(d);
+
+		// Show the tooltip box
+		d3.select('.tooltip')
+			.style('display', 'block')
+			.style('left', xPos + 'px')
+			.style('top', yPos + 'px')
+			.text(
+				`${data.date}, ${data.values['Images published with alternative text']} out of ${data.values['Images published']}`
+			);
+	}
+
+	// Function to handle mouseout event
+	function handleMouseOut() {
+		// Hide the tooltip box
+		d3.select('.tooltip').style('display', 'none');
+	}
 </script>
 
 <svelte:window bind:innerHeight bind:innerWidth />
@@ -75,6 +115,7 @@
 			</div>
 		</div>
 		<select bind:value={timerange}>
+			<option value={all}></option>
 			<option value={lastWeek}>Last week</option>
 			<option value={lastMonth}>Last month</option>
 			<option value={lastYear} selected>Last year</option>
@@ -86,16 +127,38 @@
 			<option>Opinion</option>
 		</select>
 	</div>
-
 	<figure>
+		<div class="tooltip"></div>
 		<svg {width} {height}>
 			<g bind:this={gx} transform="translate({0}, {height - padding})" />
 			<g bind:this={gy} transform="translate({padding}, {0})" />
-			{#each index as [date, values], i}
-			<g transform="translate({padding + i * 10}, {height - values["Images published with alternative text"] - padding})">
-				<rect fill="lightgreen" width={10} height={values["Images published with alternative text"]}/>
+			<g>
+				{#each index as [date, values]}
+					<g
+						style="margin: 0; padding; 0; gap: 0;"
+						on:mouseover={(d) => handleMouseOver(d, { date, values })}
+						on:mouseout={() => handleMouseOut}
+						on:focus={() => {}}
+						on:blur={() => {}}
+						role="contentinfo"
+					>
+						<rect
+							fill="lightcoral"
+							x={x(new Date(date))}
+							width={(width - padding) / (tidy.length / 2) - gap}
+							y={y(values['Images published'])}
+							height={height - padding - y(values['Images published'])}
+						/>
+						<rect
+							fill="lightgreen"
+							x={x(new Date(date))}
+							width={(width - padding) / (tidy.length / 2) - gap}
+							y={y(values['Images published with alternative text'])}
+							height={height - padding - y(values['Images published with alternative text'])}
+						/>
+					</g>
+				{/each}
 			</g>
-			{/each}
 		</svg>
 	</figure>
 </main>
@@ -107,6 +170,14 @@
 
 	figure {
 		margin: 0;
+	}
+	.tooltip {
+		position: absolute;
+		display: none;
+		padding: 5px;
+		background-color: white;
+		color: black;
+		border: 1px solid black;
 	}
 
 	.options {
@@ -132,7 +203,7 @@
 	select {
 		display: inline-block;
 		padding: 5px;
-		
+
 		font-size: 16px;
 		width: 200px;
 
