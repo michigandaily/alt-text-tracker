a72117e3436e749bf985ef81b5e542365016a651
yumarie <yumarie@umich.edu>
2024-03-12 20:07:21 -0400

    add NOT NULL attribute to date

--- migrations/001-alter.sql
+++ migrations/001-alter.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS articles (
     aid                                 INTEGER PRIMARY KEY,
-    date                                TEXT,
+    date                                TEXT NOT NULL,
     images_published                    INTEGER NOT NULL,
     images_published_with_alt_text      INTEGER NOT NULL,
     categories                          TEXT NOT NULL
