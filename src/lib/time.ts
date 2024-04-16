e133e644dcf4d9118e738ec8186e7bc0bb1df55a
yumarie <yumarie@umich.edu>
2024-04-16 19:34:58 -0400

    invalidate root url key, need to invalidate all storage in the future
    log cache status
    
    use standard js datetimeoptions type

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,14 +1,15 @@
-import type { DateStringOptions } from "./types";
-
 export const lastWeek = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
 export const lastMonth = new Date(new Date().getTime() - 31 * 24 * 60 * 60 * 1000);
 export const lastSixMonths = new Date(new Date().getTime() - 31 * 24 * 60 * 60 * 1000 * 6);
 export const lastYear = new Date(new Date().getTime() - 365 * 24 * 60 * 60 * 1000);
 export const all = new Date('2022-12-31');
 
-export function formatISODate(date: string, { weekday, year, month, day }: DateStringOptions) {
+export function formatISODate(
+	date: string,
+	{ weekday, year, month, day }: Intl.DateTimeFormatOptions
+) {
 	return new Date(date).toLocaleDateString('en-us', {
-		timeZone: "UTC",
+		timeZone: 'UTC',
 		weekday,
 		year,
 		month,
