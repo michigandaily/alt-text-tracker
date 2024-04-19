85d80b53e9ad079f4b245d27958b67b4e4f707ab
yumarie <yumarie@umich.edu>
2024-04-19 14:18:49 -0400

    change PRODUCTION comparison to be more explicit

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -98,7 +98,7 @@ export const actions: Actions = {
 		}
 
 		const cache = await platform.caches.open(D1CacheName);
-		const baseUrl = +platform.env.PRODUCTION ? url : 'http://localhost:8788';
+		const baseUrl = platform.env.PRODUCTION === "false" ? 'http://localhost:8788' : url;
 		platform.context.waitUntil(cacheInvalidate([baseUrl], cache));
 
 		redirect(304, String(data.get('path')));
